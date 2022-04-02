eae23951fc567982240fe3a78d875d2a44fdbe80
Edgar Neto <edgarneto784@gmail.com>
2022-04-01 21:59:37 -0300

    react-primeira_aula

---
+++ app-intro/src/Template.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function Usuarios() {
+    // variáveis de state
+
+
+    return (
+        <div>
+            <h3>Manutenção de Usuários</h3>
+        </div>
+        
+    );
+}
+
+export default Template;
